239cef76bd06941df6967572aa8432d31c92edfd
awmleer <awmleer@sparker.xyz>
2018-09-08 00:45:55 +0800

    use request for backend proxy

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
 
+const request = require('request');
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.response.body = request('https://api.caiyunapp.com/v2/{token}/121.6544,25.1552/forecast.json');
 });
 
 app.listen(3000);
